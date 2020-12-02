6379481cfc00b27508dd4ae9f72da48c6f2634fb
Thiago <thiagocostas2017@gmail.com>
2020-12-02 10:22:35 -0300

    finalizado

---
+++ Personagem.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace Nova_pasta
+{
+    public class Personagem
+    {
+          
+        
+        public string nome;
+        public int idade;
+        public string armadura;
+        public string IA = "Jarvis";
+        public int vida = 100;
+
+
+
+        public int Atacar1(){
+
+            return 10;
+        }
+
+        public int Atacar2(){
+
+            return 20;
+        }
+
+        public int Defender(int ataque){
+            
+            return this.vida = vida - ataque;;
+        }
+
+    }
+}
